40d5d4a5b41f2355e09862d4368d938dc49965b6
makoto-sawataki <makoto.sawataki@gmail.com>
2020-03-12 15:43:39 +0900

    add files

---
+++ MultiplicationTable.py
@@ -0,0 +1,19 @@
+# MultiplicationTable.py
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+#a = Line[0] * Column[0]
+#print(a)
+
+#a = Line[0] * Column[1]
+#print(a)
+
+#a = Line[0] * Column[2]
+#print(a)
+
+#a = Line[0] * Column[3]
+#print(a)
+
+for i in Column:
+    a = Line[0] * Column[i]
+    print(a)
